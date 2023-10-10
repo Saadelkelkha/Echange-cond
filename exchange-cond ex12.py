e3b7822589784d7bdbb24bf9cbb4c0a524c5efaf
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-10-10 22:51:16 +0100

    Add files via upload

---
+++ exchange-cond ex12.py
@@ -0,0 +1,8 @@
+a=float(input("type the first number :"))
+b=float(input("type the second number :"))
+if a*b>0 :
+    print("the first number is :",b)
+    print("the second number is :",a)
+else :
+    print("the first number is :",a+b)
+    print("the second number is :",a*b)
